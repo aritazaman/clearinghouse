e44490502ba2d76dbb4d01120ce8a40713005e6a
aritazaman <zaman.arita@gmail.com>
2026-02-25 14:04:11 -0500

    .gitignore, environment variables

--- testAPI.py
+++ testAPI.py
@@ -1,16 +1,20 @@
 import json
 import requests
+import os
+from dotenv import load_dotenv
 
-API_TOKEN = ""  
+load_dotenv()
+
+API_TOKEN = os.getenv("API_TOKEN")
 
 # url = "https://clearinghouse.net/api/v2p1/test"
+# response = requests.request("GET", url, headers=headers)
+# print(response.text)
 
 headers = {
     "Authorization": f"Token {API_TOKEN}",
     "User-Agent": "Chrome v22.2 Linux Ubuntu"
 }
-# response = requests.request("GET", url, headers=headers)
-# print(response.text)
 
 url = "https://clearinghouse.net/api/v2p1/cases"
 response = requests.get(url, headers=headers)
